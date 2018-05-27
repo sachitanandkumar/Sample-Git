10cf4ad54f6ba3f1251169071bf51658d3382693
Sachitanand Kumar <437097@FutureGroup.net>
2018-05-27 14:46:35 +0530

    added variables a and b

--- src/test/java/GitProject/Sample/DataproviderTestNg.java
+++ src/test/java/GitProject/Sample/DataproviderTestNg.java
@@ -1,7 +1,12 @@
 package GitProject.Sample;
 
 public class DataproviderTestNg {
+	int a; int b;
  public static void main(String[] args) {
-	System.out.println("hello demo");
+	System.out.println("hello User");
+	System.out.println("hello java");
+
+	System.out.println("hello book");
+
 }
 }
